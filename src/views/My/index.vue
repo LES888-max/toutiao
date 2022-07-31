326c464fad74b9cfa04e90d6af3a6e8beea4d141
LES666 <11200661+les666@user.noreply.gitee.com>
2022-07-31 11:22:38 +0800

    init

--- src/views/My/index.vue
+++ src/views/My/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>my</div>
+  <div>
+    <header>
+      <div v-if="isLogin">登录后的盒子</div>
+      <div v-else>未登录的盒子</div>
+    </header>
+    <main>
+      <div>收藏、历史、消息通知、小智同学</div>
+    </main>
+    <footer v-if="isLogin" @click="logout"><button>退出</button></footer>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    isLogin() {
+      return !!this.$store.state.tokenObj.token
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.commit('SET_TOKEN', {})
+    }
+  }
+}
 </script>
 
 <style></style>
